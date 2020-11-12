92b0e6bd25f7b1beb1cee6b1b246b8ea0164beeb
AllaBoo <a.v.buteeva@gmail.com>
2020-11-12 16:46:29 +0300

    Исправлен адрес запросов к backend

--- src/scripts/config/config.js
+++ src/scripts/config/config.js
@@ -1,3 +1,6 @@
 
 export const newsApiServer = NODE_ENV === 'development' ? 'https://newsapi.org/' : 'https://nomoreparties.co/news/';
-export const mainApiServer = NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://api.news-explorer-praktikum.ru/';
+export const mainApiServer = NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://localhost:3000';
+
+// при запущенном сервере адреса:
+//export const mainApiServer = NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.news-explorer-praktikum.ru/';
